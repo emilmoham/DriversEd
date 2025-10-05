e667493e8aabd348af3d4d0a028f6d7d41a90bdc
Emil Mohammed <emilmoham@gmail.com>
2025-10-05 10:23:30 -0400

    instances of base_car are now freed on colision

--- entities/base_car/base_car.gd
+++ entities/base_car/base_car.gd
@@ -8,7 +8,7 @@ const Explosion = preload("res://entities/explosion/explosion.tscn")
 func _ready() -> void:
 	update_color(tint)
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Engine.is_editor_hint():
 		update_color(tint)
 
@@ -20,12 +20,14 @@ func update_color(color:Color):
 	$CarNode/ColorRect/ColorRect5.color = color
 
 
-func _on_area_entered(area: Area2D) -> void:
+func _on_area_entered(_area: Area2D) -> void:
 	$CollisionShape2D.set_deferred("disabled", true)
 	
 	$CarNode.visible = false
-	var rotation = self.get_transform().get_rotation()
+	var parent_rotation = self.get_transform().get_rotation()
+	var parent_position = self.get_transform().get_origin()
 	
 	var explosion_instance = Explosion.instantiate() as Node2D
-	explosion_instance.rotation += -1 * rotation
-	self.add_child(explosion_instance)
+	explosion_instance.position = parent_position
+	get_parent().add_child(explosion_instance)
+	queue_free()
--- entities/explosion/explosion.gd
+++ entities/explosion/explosion.gd
@@ -2,3 +2,9 @@ extends Node2D
 
 func _ready() -> void:
 	$AnimationPlayer.play("explosion")
+
+
+
+func _on_animation__finished(anim_name: StringName) -> void:
+	if (anim_name == "explosion"):
+		queue_free()
