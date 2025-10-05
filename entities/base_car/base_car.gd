d2003a8e198b0e849e3c4eebf1048a670918f325
Emil Mohammed <emilmoham@gmail.com>
2025-10-04 20:36:46 -0400

    Added explosions to base car

--- entities/base_car/base_car.gd
+++ entities/base_car/base_car.gd
@@ -3,6 +3,8 @@ extends Area2D
 
 @export var tint = Color("#FFFFFF")
 
+const Explosion = preload("res://entities/explosion/explosion.tscn")
+
 func _ready() -> void:
 	update_color(tint)
 	
@@ -11,8 +13,19 @@ func _process(delta: float) -> void:
 		update_color(tint)
 
 func update_color(color:Color):
-	$ColorRect.color = color
-	$ColorRect/ColorRect2.color = color
-	$ColorRect/ColorRect3.color = color
-	$ColorRect/ColorRect4.color = color
-	$ColorRect/ColorRect5.color = color
+	$CarNode/ColorRect.color = color
+	$CarNode/ColorRect/ColorRect2.color = color
+	$CarNode/ColorRect/ColorRect3.color = color
+	$CarNode/ColorRect/ColorRect4.color = color
+	$CarNode/ColorRect/ColorRect5.color = color
+
+
+func _on_area_entered(area: Area2D) -> void:
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+	$CarNode.visible = false
+	var rotation = self.get_transform().get_rotation()
+	
+	var explosion_instance = Explosion.instantiate() as Node2D
+	explosion_instance.rotation += -1 * rotation
+	self.add_child(explosion_instance)
